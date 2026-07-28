1f107e10d64450c1450dab2a318d0c6398e0ef7c
Akash Yadav <akashyadav.fzb123@gmail.com>
2026-07-28 14:28:49 +0530

    eventloop

--- lab 1/22.js
+++ lab 1/22.js
@@ -13,6 +13,29 @@ function numberToWord(num) {
         default: return "Invalid Number";
     }
 }
+function digitToWord(digit) {
+    switch (digit) {
+        case '0': return "Zero";
+        case '1': return "One";
+        case '2': return "Two";
+        case '3': return "Three";
+        case '4': return "Four";
+        case '5': return "Five";
+        case '6': return "Six";
+        case '7': return "Seven";
+        case '8': return "Eight";
+        case '9': return "Nine";
+    }
+}
+
+let rollNo = "1209676";
+let words = "";
+
+for (let digit of rollNo) {
+    words += digitToWord(digit) + " ";
+}
+
+console.log(words.trim());
 
 console.log(numberToWord(0)); 
 console.log(numberToWord(5)); 
