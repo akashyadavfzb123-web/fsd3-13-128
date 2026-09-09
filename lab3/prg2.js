e3d26c89ceebefcf59a3dd959f72d3f3f7547102
Akash Yadav <akashyadav.fzb123@gmail.com>
2026-09-09 12:15:29 +0530

    Add README, HTML files, and server scripts for Smart Water Bottle and Indian Water Pot projects

--- lab3/prg2.js
+++ lab3/prg2.js
@@ -3,6 +3,9 @@ import http from "http";
 const server = http.createServer((req, res) => {
   res.write("<h1>Hello Client</h1>");
    res.write("<h2>akash</h2>");
+   res.write(
+
+    );
   res.end();
 });
 
---
+++ lab3/prg3.js
@@ -0,0 +1,50 @@
+import { createReadStream } from "fs";
+import http from "http";
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === "/bottle") {
+
+        res.setHeader("Content-Type", "text/html");
+
+        const stream = createReadStream("bottle.html", {
+            encoding: "utf-8"
+        });
+
+        stream.pipe(res);
+
+    } else if (req.url === "/pot") {
+
+        res.setHeader("Content-Type", "text/html");
+
+        const stream = createReadStream("pot.html", {
+            encoding: "utf-8"
+        });
+
+        stream.pipe(res);
+
+    } else {
+
+        res.statusCode = 404;
+        res.end("Page Not Found");
+    }
+});
+
+server.listen(3000, () => {
+    console.log("prg3 is running");
+});
+// import { createReadStream } from "fs";
+// import http from "http";
+
+// const server = http.createServer((req, res) => {
+//     // res.end("<h1>Hello server</h1>");
+//     if(req.url==="/bottle"){
+//     const stream = createReadStream("bottle.html",{encoding:"utf-8"});
+//     stream.pipe(res);}
+//     else{
+//         res.statuscode=408;
+//         res.end();
+//     }
+// });
+
+// server.listen(3000, () => console.log("prg3 is running"));
